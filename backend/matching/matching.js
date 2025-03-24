17d6865299d057631091641b89f708c931ab90f5
MysteriousLoner <leeyang031110@gmail.com>
2025-03-24 22:16:55 +0800

    add ping api

--- backend/matching/matching.js
+++ backend/matching/matching.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Matching service')
 })
 
+app.get('/ping', (req, res) => {
+    res.send("pong")
+})
+
 app.listen(port, () => {
   console.log(`Matching service listening on port ${port}`)
 })
